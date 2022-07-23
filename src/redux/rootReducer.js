ff86e395d558f4e7c4d787bdf448b641c0fb3907
Akash <akashathnure40@gmail.com>
2022-07-23 21:46:38 +0530

    root reducer file was wrongly added in previous commit

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,7 +2,9 @@
 import { combineReducers } from "redux";
 import  cartData  from './reducers'
 
-export default combineReducers({ cartData })
+const rootReducer= combineReducers({ cartData })
+
+export default rootReducer
 
 
 
